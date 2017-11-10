81bfc8b2a26f2da9366fe1b0f630b1db9ec9f481
MrGustavo <ghboliveira@hotmail.com>
2017-11-09 23:57:05 -0300

    Atualização da interface do deposito, ainda falta algumas partes

--- Inventory Manager/src/deposit/view/FXMLInterfaceController.java
+++ Inventory Manager/src/deposit/view/FXMLInterfaceController.java
@@ -26,15 +26,19 @@ public class FXMLInterfaceController implements Initializable {
     private Pane paneLogin;
     @FXML
     private Pane paneRegister;
+    @FXML
+    private Pane paneManager;
+    @FXML
+    private Pane paneCreateProcuct;
 
     /* Statement of TextField's */
- /* Login */
+        /* Login */
     @FXML
     private PasswordField passwordFieldPassword;
     @FXML
     private TextField textFieldCnpjLogin;
 
-    /* Register */
+        /* Register */
     @FXML
     private TextField textFieldSocialName;
     @FXML
@@ -49,32 +53,63 @@ public class FXMLInterfaceController implements Initializable {
     private TextField textFieldLocationX;
     @FXML
     private TextField textFieldLocationY;
+    @FXML
+    private PasswordField passwordFieldPass;
+    @FXML
+    private PasswordField passwordFieldPass2;
+    
+        /* Create Product */
+    @FXML
+    private TextField textFieldNameProduct;
+    @FXML
+    private TextField textFieldAmountProduct;
+    @FXML
+    private TextField textFieldPriceProduct;
 
     /* Statement of Label's */
+        /* Register */
     @FXML
     private Label labelError;
+        /* Login */
     @FXML
     private Label labelErrorLogin;
+        /* Create Product */
+    @FXML
+    private Label labelErrorCreateProduct;
 
     /* Statement of Button's */
- /* Login Buttons */
+        /* Login Buttons */
     @FXML
     private Button buttonRegisterLogin;
     @FXML
     private Button buttonLogin;
 
-    /* Register Buttons */
+        /* Register Buttons */
     @FXML
     private Button buttonRegister;
     @FXML
     private Button buttonCancel;
     
+        /* Manager */
+    @FXML
+    private Button buttonCreate;
+    @FXML
+    private Button buttonChange;
+    @FXML
+    private Button buttonRemove;
+    
+        /* Create Product */
+    @FXML
+    private Button buttonCreateProduct;
+    @FXML
+    private Button buttonCancelProduct;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }
 
-    /* Button Events Login */
+                                        /* Button Events Login */
     /**
      * Button Event register, turn the pane login in invisible and the pane
      * register in visible.
@@ -98,16 +133,17 @@ public class FXMLInterfaceController implements Initializable {
             labelErrorLogin.setVisible(true);
         } else {
             labelErrorLogin.setVisible(false);
+            paneManager.setVisible(true);
         }
     }
 
-    /* Button Events Register */
+                                        /* Button Events Register */
     /**
      * Button Event Register, if the information is correct the register is
      * done.
      */
     public void eventButtonRegister() {
-        
+
         if (textFieldSocialName.getText().trim().isEmpty()) {
             labelError.setText("ERROR: Razão Social inválido.");
             labelError.setVisible(true);
@@ -129,6 +165,12 @@ public class FXMLInterfaceController implements Initializable {
         } else if (textFieldLocationY.getText().trim().isEmpty()) {
             labelError.setText("ERROR: Posição Y inválida.");
             labelError.setVisible(true);
+        } else if (passwordFieldPass.getText().trim().isEmpty()) {
+            labelError.setText("ERROR: Digite a senha.");
+            labelError.setVisible(true);
+        } else if (!passwordFieldPass.getText().equals(passwordFieldPass2.getText())) {
+            labelError.setText("ERROR: As senhas não conferem.");
+            labelError.setVisible(true);
         } else {
             labelError.setVisible(false);
         }
@@ -140,5 +182,43 @@ public class FXMLInterfaceController implements Initializable {
     @FXML
     public void eventButtonCancel() {
         paneRegister.setVisible(false);
+    }
+    
+                                            /* Button Events Create Product */
+    
+    /**
+     * Button Event Create Product, when clicked the pane "paneCreateProcuct" is
+     * set visible.
+     */
+    @FXML
+    public void eventButtonCreate() {
+        paneCreateProcuct.setVisible(true);
+    }
+    
+    /**
+     * Button Event Cancel Product, when clicked the pane "paneCreateProcuct" is set invisible.
+     */
+    @FXML
+    public void eventButtonCancelProduct() {
+        paneCreateProcuct.setVisible(false);
+    }
+    
+    /**
+     * Button Event Create Product, when clicked the fields name, amount and price is verified.
+     */
+    @FXML
+    public void eventButtonCreateProduct(){
+        if(textFieldNameProduct.getText().trim().isEmpty()){
+            labelErrorCreateProduct.setText("ERROR: Digite um nome válido.");
+            labelErrorCreateProduct.setVisible(true);
+        } else if(textFieldAmountProduct.getText().trim().isEmpty()){
+            labelErrorCreateProduct.setText("ERROR: Digite uma quantidade de produtos.");
+            labelErrorCreateProduct.setVisible(true);
+        } else if(textFieldPriceProduct.getText().trim().isEmpty()){
+            labelErrorCreateProduct.setText("ERROR: Digite um preço.");
+            labelErrorCreateProduct.setVisible(true);
+        } else {
+            labelErrorCreateProduct.setVisible(false);
+        }
     }
 }
