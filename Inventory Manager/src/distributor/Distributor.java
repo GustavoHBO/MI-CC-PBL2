b8318ea6e6f871f35bf6c57342de994104826573
MrGustavo <ghboliveira@hotmail.com>
2017-11-22 23:52:55 -0300

    Modificações no armazenamento e recuperação dos arquivos de dados. Modificação da comunicação UDP

--- Inventory Manager/src/distributor/Distributor.java
+++ Inventory Manager/src/distributor/Distributor.java
@@ -5,10 +5,127 @@
  */
 package distributor;
 
+import distributor.controller.Controller;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author gustavo
  */
 public class Distributor {
     
+            /* Statement of the Variables */
+    private DatagramSocket socketDist;
+    private DatagramPacket datagramPacket;
+    
+    private final int PORTDIST = 55660;
+    private final int LCDPROTOCOL = 4;
+    private final int LCPROTOCOL = 2;
+    
+    private final String TOKESEPARATOR = "!=";
+    
+    private Controller controllerDist;
+    
+    byte[] receiveData = null;
+    byte[] sendData = null;
+    
+    /**
+     * Method main, start the distributor.
+     * @param args - Arguments.
+     */
+    public static void main(String[] args) {
+        try {
+            new Distributor().startDistributor();
+        } catch (IOException ex) {
+            System.out.println("Error: Não foi possível iniciar o servidor.");
+        }
+    }
+    
+    /**
+     * Start the receptions of the communications and process the all clients.
+     * @throws SocketException - If the port chose is busy or occur other error.
+     */
+    private void startDistributor() throws SocketException, IOException {
+
+        Thread thread;
+        Runnable run;
+        byte[] dataByte;
+
+                /* Starting the socket in port choose */
+        socketDist = new DatagramSocket(this.PORTDIST); // if the port is busy then a exception is launched.
+        controllerDist = Controller.getInstance(); // Get the actual controller.
+        controllerDist.readAllData();
+
+        while (true) {
+            dataByte = new byte[1024];
+            datagramPacket = new DatagramPacket(dataByte, dataByte.length);
+            socketDist.receive(datagramPacket);
+
+            run = new Runnable() {
+                @Override
+                public void run() {
+                    String data;
+                    data = new String(datagramPacket.getData());
+                    identifyAction(data);
+                }
+
+                private void identifyAction(String data) {
+                    String initCode = data.substring(0, LCPROTOCOL);
+                    int lastCodeIndex = data.lastIndexOf(initCode);
+                    if (lastCodeIndex == 0) {
+                        return;
+                    }
+                    String endCode = data.substring(lastCodeIndex, lastCodeIndex + LCPROTOCOL);
+                    if (initCode.equals(endCode)) {
+                        data = data.substring(LCPROTOCOL, lastCodeIndex);
+                        switch (initCode) {
+                            case "D0":// Test the connection.
+                                sendDatagramPacket("0xD0reply0xD0", datagramPacket.getAddress(), datagramPacket.getPort());
+                                break;
+                            case "D1":// Register a server.
+                                System.out.println("Registrando");
+                                controllerDist.registerServer(data);
+                                break;
+                            case "D2":// Solicitation a server.
+                                sendDatagramPacket("0xD2" + controllerDist.getServer() + "0xD2", datagramPacket.getAddress(), datagramPacket.getPort());
+                                break;
+                            case "D8":
+                                break;
+                            case "D9":
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            };
+            
+            thread = new Thread(run);
+            thread.start();
+        }
+
+    }
+    
+    /**
+     * Send an data string for a client with ip and a port.
+     * @param data - Data to send.
+     * @param ip - Ip of destiny.
+     * @param port - Port of destiny.
+     */
+    private void sendDatagramPacket(String data, InetAddress ip, int port) {
+        try {
+            DatagramPacket sendPacket;
+            sendData = data.getBytes();
+            sendPacket = new DatagramPacket(sendData, sendData.length, ip, port);
+            socketDist.send(sendPacket);
+        } catch (IOException ex) {
+            System.out.println("ERROR: A packet don't can to be send");
+        }
+    }
 }
--- Inventory Manager/src/distributor/controller/Controller.java
+++ Inventory Manager/src/distributor/controller/Controller.java
@@ -5,10 +5,175 @@
  */
 package distributor.controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
- *
- * @author gustavo
+ * Control the actions these server.
+ * @author Gustavo Henrique.
  */
-public class Controller {
+public class Controller implements Serializable{
     
+                                 /* Statement of Variables */
+    /* Instance of controller */
+    private static Controller controller;
+    
+    private final String STRINGSAVESEPARATOR = "!=";
+    
+    private ArrayList<String> listServers;
+    
+    private Iterator<String> iteratorDist;
+    
+                            /* Design Pattern Singleton */
+    
+    /* The constructor is private for use the singleton */
+    private Controller(){
+        listServers = new ArrayList<>();
+        try {
+            readAllData();
+        } catch (IOException ex) {
+            System.out.println("ERROR: Não foi possível ler os dados armazenados.");
+        }
+    }
+    
+    /**
+     * Return the instance of controller.
+     * @return controller - An instance.
+     */
+    public static Controller getInstance(){
+        if(controller == null){
+            controller = new Controller();
+        }
+        return controller;
+    }
+    
+    /**
+     * Reset the controller.
+     */
+    public static void resetController(){
+        controller = null;
+    }
+    
+                                                /* End Singleton */
+                                                    /* Methods */
+    
+    /**
+     * Register the server.
+     * @param server - Server to register.
+     */
+    public void registerServer(String server){
+        if(findServer(server) == null){
+            listServers.add(server);
+            try {
+                saveAllData();
+            } catch (IOException ex) {
+                System.out.println("ERROR: Não foi possível armazenar os dados.");
+            }
+        }
+    }
+    
+    /**
+     * Search the server using the data of the server.
+     * @param server - Server to search.
+     * @return null - If the server not exist, server - If the server exist.
+     */
+    private String findServer(String server){
+        Iterator<String> it = listServers.iterator();
+        
+        while(it.hasNext()){
+            if(it.next().equals(server)){
+                return server;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Return the serve that will to receive the connection.
+     * @return null - If don't have servers registered, server - Server that will to receive the connection.
+     */
+    public String getServer(){
+        if(iteratorDist == null || !iteratorDist.hasNext()){
+            iteratorDist = listServers.iterator();
+        }
+        return iteratorDist.next();
+    }
+    
+                                               /* Methods in storage */
+    /**
+     * Save the data in an archive .im
+     * @return 0 - If the list is null or empty, 1 case the data was save.
+     * @throws IOException - If the archive can't be create.
+     */
+    public int saveAllData() throws IOException {
+        OutputStream os;
+        OutputStreamWriter osw;
+        BufferedWriter bw;
+        Iterator<String> it;
+
+        File file = new File("./backup/distributor/servers");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        file = new File("./backup/distributor/servers/data.im");
+        if (file.exists()) {// Delete the archive for that to save the new.
+            file.delete();
+        }
+        file.createNewFile();
+        
+        if(listServers == null){
+            return 0;
+        } else if (listServers.isEmpty()){
+            return 0;
+        } else {
+            os = new FileOutputStream(file);
+            osw = new OutputStreamWriter(os);
+            bw = new BufferedWriter(osw);
+            it = listServers.iterator();
+            while (it.hasNext()) {
+                bw.write(it.next());
+                if(it.hasNext()){// If have next, then create new line for next server.
+                    bw.newLine();
+                }
+            }
+            bw.close();
+            osw.close();
+            os.close();
+        }
+        return 1;
+    }
+
+    /**
+     * Read all data in storage and put in a list.
+     * @throws IOException - If the file can't to be read.
+     */
+    public void readAllData() throws IOException {
+        File file = new File("./backup/distributor/servers/data.im");
+        if (file.exists()) {
+            FileReader fileReader;
+            fileReader = new FileReader(file);// FileReader it read the archive.
+            BufferedReader bufferedReader;
+            bufferedReader = new BufferedReader(fileReader);//BufferedReader it store the data read in a buffer.
+
+            if (listServers == null) {
+                listServers = new ArrayList<>();
+            }
+
+            while (bufferedReader.ready()) {
+                listServers.add(bufferedReader.readLine());
+            }
+            bufferedReader.close();
+            fileReader.close();
+        }
+    }
 }
