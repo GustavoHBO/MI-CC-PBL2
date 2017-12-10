f68b57e3a5cef3f5a5f7c14814d6cc9aad68b9a4
MrGustavo <ghboliveira@hotmail.com>
2017-12-10 18:01:26 -0300

    Contagem do id automática

---
+++ Inventory Manager/src/deposit/model/Deposit.java
@@ -0,0 +1,192 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ */
+package deposit.model;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Deposit {
+    
+    private String socialName;
+    private String cpf;
+    private String number;
+    private String fantasyName;
+    private String cnpj;
+    private String password;
+    private int posX;
+    private int posY;
+    
+    /**
+     * Constructor of this class.
+     * @param cpf
+     * @param cnpj
+     * @param socialName
+     * @param fantasyName
+     * @param number
+     * @param password
+     * @param posX
+     * @param posY 
+     */
+    public Deposit(String cpf, String cnpj, String socialName, String fantasyName, String number, String password, int posX, int posY){
+        
+        this.cpf = cpf;
+        this.cnpj = cnpj;
+        this.socialName = socialName;
+        this.fantasyName = fantasyName;
+        this.number = number;
+        this.password = password;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+                                                /* Encapsulate Fields */
+    /**
+     * @return the socialName
+     */
+    public String getSocialName() {
+        return socialName;
+    }
+
+    /**
+     * @param socialName the socialName to set
+     */
+    public void setSocialName(String socialName) {
+        this.socialName = socialName;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the fantasyName
+     */
+    public String getFantasyName() {
+        return fantasyName;
+    }
+
+    /**
+     * @param fantasyName the fantasyName to set
+     */
+    public void setFantasyName(String fantasyName) {
+        this.fantasyName = fantasyName;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the posX
+     */
+    public int getPosX() {
+        return posX;
+    }
+
+    /**
+     * @param posX the posX to set
+     */
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    /**
+     * @return the posY
+     */
+    public int getPosY() {
+        return posY;
+    }
+
+    /**
+     * @param posY the posY to set
+     */
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+}
